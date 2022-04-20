03924348020cd17b38a7389b0326968e05fc22b0
yjara v <70939994+yjara@users.noreply.github.com>
2022-04-20 16:08:50 +0200

    added style.css, About.html, errorPage.html, base.html & Homepage.html

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+import io
+
+from flask import Flask,request,render_template
+from Bio import Entrez
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',methods=["POST", "GET"])
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    if request.method == "POST":
+        answer = request.form.get("answer", "")
+        return render_template("Homepage.html", answer=answer)
+    else:
+        return render_template("Homepage.html", answer="")
+
+@app.route('/About',methods=["POST", "GET"])
+def info():
+    return render_template("About.html")
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("errorPage.html")
 
 
 if __name__ == '__main__':
