a9e7299a07190eb99e351014921083cc3f898e23
yjara v <70939994+yjara@users.noreply.github.com>
2022-05-10 23:09:30 +0200

    added start.html

--- app.py
+++ app.py
@@ -5,14 +5,21 @@ from Bio import Entrez
 
 app = Flask(__name__)
 
-
 @app.route('/',methods=["POST", "GET"])
+def start():
+    return render_template("start.html")
+
+@app.route('/Homepage',methods=["POST", "GET"])
 def hello_world():  # put application's code here
     if request.method == "POST":
-        answer = request.form.get("answer", "")
-        return render_template("Homepage.html", answer=answer)
+        file = request.form.get("file", "")
+
+        if not file.endswith(".xlsx"):
+            file = "Please select an excel file"
+        print(file)
+        return render_template("Homepage.html", file=file)
     else:
-        return render_template("Homepage.html", answer="")
+        return render_template("Homepage.html", file="")
 
 @app.route('/About',methods=["POST", "GET"])
 def info():
