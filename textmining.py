8c20a6eb3b8420c1b105dceb8459f5776a401a31
yjara v <70939994+yjara@users.noreply.github.com>
2022-05-25 19:36:22 +0200

    textmining updated layout

--- textmining.py
+++ textmining.py
@@ -1,19 +1,9 @@
-import threading
-import time
-from datetime import datetime
-import re
-import nltk
 from nltk.corpus import wordnet
 #nltk.download('wordnet')
 #nltk.download('omw-1.4')
 from Bio import Entrez
-import urllib
-
 import requests
-import io
-import json
-import sys
-import fileReader
+
 
 
 pub_disease = 0   #disease id number
@@ -21,13 +11,14 @@ pub_Om = 0 #Omim ID
 metab = 0 #metabolite key
 
 
-
-
-
-
 def get_synonyms(word):
-    synonyms = []
+    """ Obtains synonyms from a word and makes one string of them in the
+    following format: word1 OR word2 OR word3
 
+    :param word: string, the word we want the synonyms from
+    :return: string of the synonyms seperated by " OR "
+    """
+    synonyms = []
     for syn in wordnet.synsets(word):
         for l in syn.lemmas():
             synonyms.append(l.name())
@@ -38,82 +29,96 @@ def get_synonyms(word):
         stringSyn = word
     return stringSyn
 
-def get_ids(combie):
-    startdate = 2000
-    maxDate = 2022
-    date = startdate
-    handle = Entrez.esearch(db='pubmed', term=f"{combie}[Title/Abstract]",
-                            mindate=f'{date}/01/01',
-                            maxdate=f'{maxDate}/12/31')
-    record = Entrez.read(handle)
-    handle.close()
-    idlist = record["IdList"]
-    return idlist
-
 def get_ids_all_pubmed(metabolites):
+    """Puts pubmed id's from artikels containing metabolite in list (idlist).
+    Calls function get_title_diseases with as parameter idlist.
+    (This is done for all metabolites)
+
+    :param metabolites: list with all metabolites
+    """
+    global metab
     Entrez.email = 'A.C.Other@example.com'
     for item in metabolites:
         startdate = 2000
-        date = startdate
         handle = Entrez.esearch(db='pubmed', term=f"{get_synonyms(item)}[Title/Abstract]",
-                                mindate=f'{date}/01/01')
+                                mindate=f'{startdate}/01/01')
         record = Entrez.read(handle)
         handle.close()
         idlist = record["IdList"]
         print(f">>>{item}<<<<<")
-
-        global metab
+        # This is done to keep count of the metabolites
         metab +=1
+        # To obtain information from the articles
         get_title_diseases(idlist)
 
-#Abstract gene, Geïndexeerd met Medical Subject Headings (MeSH)
 def get_title_diseases(ids):
-    # submit request
+    """ Uses Pubtator to obtain data from pubmed articles.
+    Calls info_per_article with as parameter the raw data
+
+    :param ids: list with PMIDs
+    """
+    # pubtator parameters
     Format = "pubtator"
     Type = "pmids"
     Bioconcepts = "disease"
     identities = ''.join([str(elem + ",") for elem in ids])
 
+    # web queri
     r = requests.get(
         f"https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/{Format}?{Type}={identities[0:len(identities)-1]}&concepts={Bioconcepts}")
 
-    #print(r)
+    if str(r) == "<Response [200]>":
+        # to get the wanted information out the data of the pubmed articles
+        info_per_article(r.text.split("\n"))
+    else:
+        print("Request was unsuccesfull")
 
-    info_per_article(r.text.split("\n"))
-    return r.text.split("\n")
+
 
 def info_per_article(info):
-    global pub_disease
+    """ Obtains the wanted information out the data from Pubtator,
+    and calls the functions to places this data correctly in the database
+
+    :param info: list, with the data output from Pubtator
+    """
+    # to keep count of the articles
     global pub_Om
+    # to keep count of the diseases
+    global pub_disease
+
+    # dictionary with as key PUMID, and values title (string)
+    # and diseases (list)
     articles = {}
     diseases = []
+    # Medical Subject Headings (MeSH)
     mushs = {}
     title = ""
     id_n = ""
-    id = ""
     for item in info:
+        # PMID
         id = item[0:8]
         if id == "" and title != "":
             if not diseases == []:
-                pub_Om +=1
-                metab_key = metab   #secondary key Metabolieten
-                id_pum_om = pub_Om  #primary key PubOm
+                pub_Om += 1
+
+                # primary key PubOm
+                id_pum_om = pub_Om
                 PMID = id_n
                 article_name = title
+                # fill pubOM table (database)
+                #######fill_PubOM(id_pum_om, PMID, article_name)
 
-                #fill pubOM table (database)
-                fill_PubOM(id_pum_om, PMID, article_name)
-
-                # Obtain how frequent an diseases occurs in text
+                # Obtain how frequent a diseases occurs in text
+                # fills pub_disease
                 diseases_freq_article(diseases, mushs)
 
-                #adds key, article and diseases to dict
-                articles[id_n] = [title,diseases]
-                ##hier vullen we gelijk de table pubmed (database)
+                articles[id_n] = [title, diseases]
                 diseases = []
         if item[9:11] == "t|":
+            # obtain article title
             title = item.split("t|")[1]
         elif item.startswith(f"{id}\t"):
+            # obtain diseases and MeSH
             items = item.split("\t")
             if items[5] == "":
                 mesh = "NULL"
@@ -124,16 +129,24 @@ def info_per_article(info):
             diseases.append(item.split("\t")[3].lower())
         id_n = item[0:8]
 
-def diseases_freq_article(diseases,mushs):
+def diseases_freq_article(diseases, mushs):
+    """ Makes an dictionary with as key the disease and value the number of
+    times that diseases was found in the article. And calls the function
+    fill_pub_disease with as parameters counts (dict) and mushs (dict)
+
+    :param diseases: list with diseases in article
+    :param mushs: disctionary with diseases + MuSH
+    """
     counts = {}
     for disease in diseases:
-        counts[disease] = counts.get(disease,0)+1
-    fill_pub_disease(counts,mushs)
+        counts[disease] = counts.get(disease, 0)+1
+    # fills pub_disease
+    fill_pub_disease(counts, mushs)
 
 
 def fill_pub_disease(diseases_counts,mushs):
     for key, value in diseases_counts.items(): #werkt dan niet met een primary key??!! of zo lijkt het
-       print(f"pubO: {pub_Om}, disease:{key}, count:{value}, MESH_code: {mushs.get(key)}")
+       print(f"pubOm: {pub_Om}, disease:{key}, count:{value}, MESH_code: {mushs.get(key)}")
 
 def fill_PubOM(id_pum_om,PMID,article_name):
     print(">>>PubOM<<<<")
