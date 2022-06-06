820f38d9c52a13df6a0213aeea917191906581a4
Nienke <nienke.gertsen@xs4all.nl>
2022-06-06 15:47:59 +0200

    patient en metabolieten werkend

---
+++ test.py
@@ -0,0 +1,107 @@
+import plotly.express as px
+import psycopg2
+
+char = []
+par = []
+values = []
+
+def get_meta_top(conn, cursor):
+    global char
+    global par
+    global values
+    patient = "P1002.1_Zscore"
+    val = -100
+    val2 = 100
+    postgre = ("""SELECT id_patient, name FROM metabolieten
+      JOIN z_scores ON metabolieten.id_metaboliet=z_scores.metabolieten_id_metaboliet
+      JOIN patients ON z_scores.patients_id_patient=patients.id_patient
+      WHERE id_patient='{}' AND (z_score < {} OR z_score > {})
+      ORDER BY z_score desc
+      LIMIT 10;""").format(patient,
+        val, val2)
+    cursor.execute(postgre)
+    result = cursor.fetchall()
+
+    for x in result:
+        print(x)
+        pat = x[0]
+        meta = x[1]
+        char.append(str(meta))
+
+    i = 0
+    while i <= len(result):
+        par.append(str(pat))
+        i += 1
+
+
+def get_meta_bottom(conn, cursor):
+    global char
+    global par
+    global values
+    patient = "P1002.1_Zscore"
+    val = -100
+    val2 = 100
+    postgre = ("""SELECT id_patient, name FROM metabolieten
+      JOIN z_scores ON metabolieten.id_metaboliet=z_scores.metabolieten_id_metaboliet
+      JOIN patients ON z_scores.patients_id_patient=patients.id_patient
+      WHERE id_patient='{}' AND (z_score < {} OR z_score > {})
+      ORDER BY z_score asc
+      LIMIT 10;""").format(patient,
+        val, val2)
+    cursor.execute(postgre)
+    result = cursor.fetchall()
+
+    for x in result:
+        #print(x)
+        pat = x[0]
+        meta = x[1]
+        char.append(str(meta))
+
+    i = 1
+    while i < len(result):
+        par.append(str(pat))
+        i += 1
+
+
+
+def make_figure():
+    global char
+    global par
+    global values
+
+    for z in char:
+        values.append(char.count(str(z)))
+
+    # print(len(char))
+    # print(len(par))
+    # print(len(values))
+    #
+    # print(char)
+    # print(par)
+    # print(values)
+    data = dict(
+        character=char, parent=par, value=values)
+
+    fig = px.sunburst(
+        data,
+        names='character',
+        parents='parent',
+        values='value',
+    )
+    fig.show()
+
+
+
+
+
+if __name__ == '__main__':
+    # connect aan de database
+    conn = psycopg2.connect(host="postgres.biocentre.nl", user="BI2_PG1", password="blaat1234",
+                            database="bio_jaar_2_pg_1", port="5900")
+
+    # open een cursor
+    cursor = conn.cursor()
+    get_meta_top(conn, cursor)
+    get_meta_bottom(conn, cursor)
+
+    make_figure()
