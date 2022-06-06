30d16e56a8375cbed0e117510d9bd9742c72c2fe
Nienke <nienke.gertsen@xs4all.nl>
2022-06-06 18:03:36 +0200

    patient en metabolieten en diseases werkend

--- test.py
+++ test.py
@@ -4,11 +4,13 @@ import psycopg2
 char = []
 par = []
 values = []
+metabolits = []
 
 def get_meta_top(conn, cursor):
     global char
     global par
     global values
+    global metabolits
     patient = "P1002.1_Zscore"
     val = -100
     val2 = 100
@@ -17,7 +19,7 @@ def get_meta_top(conn, cursor):
       JOIN patients ON z_scores.patients_id_patient=patients.id_patient
       WHERE id_patient='{}' AND (z_score < {} OR z_score > {})
       ORDER BY z_score desc
-      LIMIT 10;""").format(patient,
+      LIMIT 7;""").format(patient,
         val, val2)
     cursor.execute(postgre)
     result = cursor.fetchall()
@@ -27,6 +29,10 @@ def get_meta_top(conn, cursor):
         pat = x[0]
         meta = x[1]
         char.append(str(meta))
+        if meta not in metabolits:
+            metabolits.append(meta)
+        else:
+            pass
 
     i = 0
     while i <= len(result):
@@ -38,15 +44,16 @@ def get_meta_bottom(conn, cursor):
     global char
     global par
     global values
+    global metabolits
     patient = "P1002.1_Zscore"
     val = -100
     val2 = 100
     postgre = ("""SELECT id_patient, name FROM metabolieten
       JOIN z_scores ON metabolieten.id_metaboliet=z_scores.metabolieten_id_metaboliet
       JOIN patients ON z_scores.patients_id_patient=patients.id_patient
-      WHERE id_patient='{}' AND (z_score < {} OR z_score > {})
+      WHERE id_patient LIKE'{}%' AND (z_score < {} OR z_score > {})
       ORDER BY z_score asc
-      LIMIT 10;""").format(patient,
+      LIMIT 7;""").format(patient,
         val, val2)
     cursor.execute(postgre)
     result = cursor.fetchall()
@@ -56,6 +63,10 @@ def get_meta_bottom(conn, cursor):
         pat = x[0]
         meta = x[1]
         char.append(str(meta))
+        if meta not in metabolits:
+            metabolits.append(meta)
+        else:
+            pass
 
     i = 1
     while i < len(result):
@@ -67,58 +78,86 @@ def get_disease_top(cursor):
     global char
     global par
     global values
-    patient = "P1002.1_Zscore"
-    val = -100
-    val2 = 100
-    postgre = ("""SELECT name, disease, count FROM metabolieten
-      JOIN z_scores ON metabolieten.id_metaboliet=z_scores.metabolieten_id_metaboliet
-      JOIN patients ON z_scores.patients_id_patient=patients.id_patient
-      JOIN metabolieten_pubom mp ON metabolieten.id_metaboliet = mp.metabolieten_id_metaboliet
-      JOIN pubom ON mp.pubom_id_pum_om = pubom.id_pum_om
-      JOIN pub_disease_pubom pdp ON pubom.id_pum_om = pdp.pubom_id_pum_om
-      JOIN pub_disease ON pdp.pub_disease_id_article = pub_disease.id_article
-      WHERE id_patient='{}' AND (z_score < {} OR z_score > {})
-      ORDER BY z_score desc;""").format(patient, val, val2)
-    cursor.execute(postgre)
-    result = cursor.fetchall()
+    global metabolits
+    for meta in metabolits:
+        patient = "P1002.1_Zscore"
+        val = -100
+        val2 = 100
+        postgre = ("""SELECT name, disease, count FROM metabolieten
+          JOIN z_scores ON metabolieten.id_metaboliet=z_scores.metabolieten_id_metaboliet
+          JOIN patients ON z_scores.patients_id_patient=patients.id_patient
+          JOIN metabolieten_pubom mp ON metabolieten.id_metaboliet = mp.metabolieten_id_metaboliet
+          JOIN pubom ON mp.pubom_id_pum_om = pubom.id_pum_om
+          JOIN pub_disease_pubom pdp ON pubom.id_pum_om = pdp.pubom_id_pum_om
+          JOIN pub_disease ON pdp.pub_disease_id_article = pub_disease.id_article
+          WHERE id_patient LIKE'{}%' AND (z_score < {} OR z_score > {}) AND name LIKE '{}'
+          ORDER BY count
+          LIMIT 5;""").format(patient, val, val2, meta)
+        cursor.execute(postgre)
+        result = cursor.fetchall()
 
-    for x in result:
-        # print(x)
-        met = x[0]
-        dis = x[1]
-        cnt = x[2]
-        char.append(str(dis))
-        par.append(met)
-        values.append(cnt)
+        for x in result:
+            print(x)
+            met = x[0]
+            dis = x[1]
+            cnt = x[2]
+            char.append(str(dis))
+            par.append(met)
+            values.append(cnt)
 
 
-def get_disease_bottom(cursor):
+def get_genes_top(cursor):
     global char
     global par
     global values
-    patient = "P1002.1_Zscore"
-    val = -100
-    val2 = 100
-    postgre = ("""SELECT name, disease, count FROM metabolieten
-      JOIN z_scores ON metabolieten.id_metaboliet=z_scores.metabolieten_id_metaboliet
-      JOIN patients ON z_scores.patients_id_patient=patients.id_patient
-      JOIN metabolieten_pubom mp ON metabolieten.id_metaboliet = mp.metabolieten_id_metaboliet
-      JOIN pubom ON mp.pubom_id_pum_om = pubom.id_pum_om
-      JOIN pub_disease_pubom pdp ON pubom.id_pum_om = pdp.pubom_id_pum_om
-      JOIN pub_disease ON pdp.pub_disease_id_article = pub_disease.id_article
-      WHERE id_patient='{}' AND (z_score < {} OR z_score > {})
-      ORDER BY z_score asc;""").format(patient, val, val2)
+    postgre = ("""SELECT disease, genes, pub_genes.count from pub_genes
+        JOIN pub_disease_pub_genes pdpg on pub_genes.id_artikel = pdpg.pub_genes_id_artikel
+        JOIN pub_disease pd on pdpg.pub_disease_id_article = pd.id_article;""")
     cursor.execute(postgre)
     result = cursor.fetchall()
 
     for x in result:
         # print(x)
-        met = x[0]
-        dis = x[1]
-        cnt = x[2]
-        char.append(str(dis))
-        par.append(met)
-        values.append(cnt)
+        dise = x[0]
+        gen = x[1]
+        cnt2 = x[2]
+        if dise in char:
+            char.append(str(gen))
+            par.append(str(dise))
+            values.append(cnt2)
+        else:
+            pass
+
+        print(char)
+        print(par)
+        print(values)
+
+
+def get_genes(cursor):
+    global char
+    global par
+    global values
+    postgre = ("""SELECT disease, genes, pub_genes.count from pub_genes
+        JOIN pub_disease_pub_genes pdpg on pub_genes.id_artikel = pdpg.pub_genes_id_artikel
+        JOIN pub_disease pd on pdpg.pub_disease_id_article = pd.id_article;;""")
+    cursor.execute(postgre)
+    result = cursor.fetchall()
+
+    for x in result:
+        # print(x)
+        dise = x[0]
+        gen = x[1]
+        cnt2 = x[2]
+        if dise in char:
+            char.append(str(gen))
+            par.append(str(dise))
+            values.append(cnt2)
+        else:
+            pass
+
+    print(char)
+    print(par)
+    print(values)
 
 
 def make_figure():
@@ -146,9 +185,6 @@ def make_figure():
     fig.show()
 
 
-
-
-
 if __name__ == '__main__':
     # connect aan de database
     conn = psycopg2.connect(host="postgres.biocentre.nl", user="BI2_PG1", password="blaat1234",
@@ -163,5 +199,6 @@ if __name__ == '__main__':
         values.append(char.count(str(z)))
 
     get_disease_top(cursor)
-    get_disease_bottom(cursor)
+
+    #get_genes(cursor)
     make_figure()
