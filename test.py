9dad8c9c5d0f36a8c09e1ccad36e70938bc2a9bb
Nienke <nienke.gertsen@xs4all.nl>
2022-06-06 16:19:41 +0200

    patient en metabolieten en diseases werkend

--- test.py
+++ test.py
@@ -23,7 +23,7 @@ def get_meta_top(conn, cursor):
     result = cursor.fetchall()
 
     for x in result:
-        print(x)
+        # print(x)
         pat = x[0]
         meta = x[1]
         char.append(str(meta))
@@ -52,7 +52,7 @@ def get_meta_bottom(conn, cursor):
     result = cursor.fetchall()
 
     for x in result:
-        #print(x)
+        # print(x)
         pat = x[0]
         meta = x[1]
         char.append(str(meta))
@@ -63,14 +63,68 @@ def get_meta_bottom(conn, cursor):
         i += 1
 
 
+def get_disease_top(cursor):
+    global char
+    global par
+    global values
+    patient = "P1002.1_Zscore"
+    val = -100
+    val2 = 100
+    postgre = ("""SELECT name, disease, count FROM metabolieten
+      JOIN z_scores ON metabolieten.id_metaboliet=z_scores.metabolieten_id_metaboliet
+      JOIN patients ON z_scores.patients_id_patient=patients.id_patient
+      JOIN metabolieten_pubom mp ON metabolieten.id_metaboliet = mp.metabolieten_id_metaboliet
+      JOIN pubom ON mp.pubom_id_pum_om = pubom.id_pum_om
+      JOIN pub_disease_pubom pdp ON pubom.id_pum_om = pdp.pubom_id_pum_om
+      JOIN pub_disease ON pdp.pub_disease_id_article = pub_disease.id_article
+      WHERE id_patient='{}' AND (z_score < {} OR z_score > {})
+      ORDER BY z_score desc;""").format(patient, val, val2)
+    cursor.execute(postgre)
+    result = cursor.fetchall()
+
+    for x in result:
+        # print(x)
+        met = x[0]
+        dis = x[1]
+        cnt = x[2]
+        char.append(str(dis))
+        par.append(met)
+        values.append(cnt)
+
+
+def get_disease_bottom(cursor):
+    global char
+    global par
+    global values
+    patient = "P1002.1_Zscore"
+    val = -100
+    val2 = 100
+    postgre = ("""SELECT name, disease, count FROM metabolieten
+      JOIN z_scores ON metabolieten.id_metaboliet=z_scores.metabolieten_id_metaboliet
+      JOIN patients ON z_scores.patients_id_patient=patients.id_patient
+      JOIN metabolieten_pubom mp ON metabolieten.id_metaboliet = mp.metabolieten_id_metaboliet
+      JOIN pubom ON mp.pubom_id_pum_om = pubom.id_pum_om
+      JOIN pub_disease_pubom pdp ON pubom.id_pum_om = pdp.pubom_id_pum_om
+      JOIN pub_disease ON pdp.pub_disease_id_article = pub_disease.id_article
+      WHERE id_patient='{}' AND (z_score < {} OR z_score > {})
+      ORDER BY z_score asc;""").format(patient, val, val2)
+    cursor.execute(postgre)
+    result = cursor.fetchall()
+
+    for x in result:
+        # print(x)
+        met = x[0]
+        dis = x[1]
+        cnt = x[2]
+        char.append(str(dis))
+        par.append(met)
+        values.append(cnt)
+
 
 def make_figure():
     global char
     global par
     global values
-
-    for z in char:
-        values.append(char.count(str(z)))
 
     # print(len(char))
     # print(len(par))
@@ -79,6 +133,7 @@ def make_figure():
     # print(char)
     # print(par)
     # print(values)
+
     data = dict(
         character=char, parent=par, value=values)
 
@@ -104,4 +159,9 @@ if __name__ == '__main__':
     get_meta_top(conn, cursor)
     get_meta_bottom(conn, cursor)
 
+    for z in char:
+        values.append(char.count(str(z)))
+
+    get_disease_top(cursor)
+    get_disease_bottom(cursor)
     make_figure()
