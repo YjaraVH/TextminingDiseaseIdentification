5698c647356524437426314cf8f2027d82eb9bdc
Nienke <nienke.gertsen@xs4all.nl>
2022-06-07 09:38:58 +0200

    opmaak aangepast

--- visualisatie.py
+++ visualisatie.py
@@ -5,20 +5,20 @@ import psycopg2
     Date created: 6/06/2022
 '''
 
-def info_patient_ophalen(conn, cursor):
+
+def info_patient_ophalen(cursor):
     """
     haalt informatie uit de database op die het verband weergeeft tussen de patienten,
     metabolieten, ziektes gevonden m.b.v. pubtator die gekoppeld kunnen worden aan bepaalde
     metabolieten en tot slot de z-score die zegt over de waarschijnlijkheid dat een bepaalde
     metaboliet verantwoordelijk is voor een bepaalde ziekte.
-    :param conn: connection
     :param cursor: cursor.
     """
     # default waarde om te testen
     value = -100
     value2 = 100
     patient_name = "P1002.1_Zscore"
-    filter = "desc"
+    fil = "desc"
     # definieert de query voor het ophalen.
     postgre = ("""SELECT id_patient, name, disease, z_score FROM metabolieten
       JOIN z_scores ON metabolieten.id_metaboliet=z_scores.metabolieten_id_metaboliet
@@ -29,8 +29,7 @@ def info_patient_ophalen(conn, cursor):
       JOIN pub_disease ON pdp.pub_disease_id_article = pub_disease.id_article
       WHERE id_patient='{}' AND (z_score < {} OR z_score > {})
       ORDER BY z_score {}
-      LIMIT 10;""").format(patient_name,
-        value, value2, filter)
+      LIMIT 10;""").format(patient_name, value, value2, fil)
     # voert de query uit
     cursor.execute(postgre)
     # slaat alle resultaten op onder result
@@ -38,7 +37,7 @@ def info_patient_ophalen(conn, cursor):
     # definieert een lijst om alle informatie van alle patienten met significante metabolieten op te slaan.
     info_pat = []
     for i in result:
-        #print(i)
+        # print(i)
         # definieert een lijst per patient.
         row = []
         name = i[1]
@@ -50,17 +49,16 @@ def info_patient_ophalen(conn, cursor):
         info_pat.append(info_pat)
 
 
-def info_meta_ophalen(conn, cursor):
+def info_meta_ophalen(cursor):
     """
     haalt informatie uit de database op die het verband weergeeft tussen de metaboliet naam, origin, relevantie en
     weergeeft hierbij ook de description en de hmdb-code.
-    :param conn: connection
     :param cursor: cursor
     """
     # default waarde om te testen
     value = -100
     value2 = 100
-    filter = "desc"
+    filt = "desc"
     # definieert query voor ophalen
     postgre = ("""SELECT name, origin_name, description, hmbd_code, relevance FROM metabolieten
      JOIN origins_metabolieten ON metabolieten.id_metaboliet=origins_metabolieten.metabolieten_id_metaboliet
@@ -69,7 +67,7 @@ def info_meta_ophalen(conn, cursor):
      JOIN z_scores ON metabolieten.id_metaboliet=z_scores.metabolieten_id_metaboliet
      WHERE (z_score < {} OR z_score > {})
      ORDER BY z_score {}
-     LIMIT 10;""").format(value, value2, filter)
+     LIMIT 10;""").format(value, value2, filt)
     # voert de query uit.
     cursor.execute(postgre)
     # slaat alle resultaten op onder result
@@ -108,14 +106,13 @@ def info_meta_ophalen(conn, cursor):
         info_met.append(row)
 
 
-def get_patients(conn, cursor):
+def get_patients(cursor):
     """
     Haalt alle patienten op uit de database.
-    :param conn: connection
     :param cursor: cursor
     """
     # definieert query voor ophalen
-    postgre = ("""SELECT id_patient FROM patients;""")
+    postgre = """SELECT id_patient FROM patients;"""
     # voert query uit
     cursor.execute(postgre)
     # slaat alle resultaten op onder result
@@ -125,13 +122,14 @@ def get_patients(conn, cursor):
     for i in result:
         print(i)
 
+
 if __name__ == '__main__':
     # connect aan de database
-    conn = psycopg2.connect(host="postgres.biocentre.nl", user="BI2_PG1", password="blaat1234",
-                            database="bio_jaar_2_pg_1", port="5900")
+    connect = psycopg2.connect(host="postgres.biocentre.nl", user="BI2_PG1", password="blaat1234",
+                               database="bio_jaar_2_pg_1", port="5900")
 
     # open een cursor
-    cursor = conn.cursor()
-    info_patient_ophalen(conn, cursor)
-    #info_meta_ophalen(conn, cursor)
-    #get_patients(conn, cursor)
+    curs = connect.cursor()
+    info_patient_ophalen(curs)
+    # info_meta_ophalen(cursor)
+    # get_patients(cursor)
